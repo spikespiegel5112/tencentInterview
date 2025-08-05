5a2fee492e0113dbefeb2f820c1f2bf4b2ab7e4b
wangbolun <583288277@qq.com>
2025-08-05 13:09:20 +0800

    temp

--- src/pc/gulpfile.js
+++ src/pc/gulpfile.js
@@ -1,46 +1,59 @@
-'use strict';
+"use strict";
 
-var gulp = require('gulp');
-var spritesmith = require('gulp.spritesmith');
-var sass = require('gulp-sass');
+const { src, dest, watch, series, parallel } = require("gulp");
+const spritesmith = require("gulp.spritesmith");
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const sourcemaps = require("gulp-sourcemaps");
+const autoprefixer = require("autoprefixer");
+const merge = require("merge-stream");
+// 生成雪碧图
+function spriteTask(done) {
+  const spriteData = src("image/sprite-fragment/*.png").pipe(
+    spritesmith({
+      imgName: "sprite.png",
+      cssName: "../css/sprite.css",
+      padding: 5,
+      algorithm: "binary-tree",
+    })
+  );
 
-gulp.task('sprite', function () {
-    gulp.src('image/sprite-fragment/*.png')
-        .pipe(spritesmith({
-            imgName: 'sprite.png',
-            cssName: '../css/sprite.css',
-            padding: 5,
-            algorithm: 'binary-tree'
-        }))
-        .pipe(gulp.dest('image')) //输出目录
-});
+  const imgStream = spriteData.img.pipe(dest("image"));
+  const cssStream = spriteData.css.pipe(dest("image"));
+  done();
+  return merge(imgStream, cssStream);
+}
 
-gulp.task('sass', function () {
-    gulp.src('./sass/**/*.scss')
-        .pipe(sass.sync().on('error', sass.logError))
-        .pipe(gulp.dest('./css'))
+// 编译 SASS
+function sassTask() {
+  return src("./sass/**/*.scss")
+    .pipe(sass.sync().on("error", sass.logError))
+    .pipe(dest("./css"));
+}
 
-});
+// PostCSS 自动前缀
+function postcssTask() {
+  return src("./css/*.css")
+    .pipe(sourcemaps.init())
+    .pipe(
+      postcss([
+        autoprefixer({
+          overrideBrowserslist: ["ie 7-11", "Firefox > 20", "last 2 versions"],
+        }),
+      ])
+    )
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("./dest"));
+}
 
-gulp.task('postcss', function () {
-    var postcss      = require('gulp-postcss');
-    var sourcemaps   = require('gulp-sourcemaps');
-    var autoprefixer = require('autoprefixer');
+// 监听任务
+function watchTask() {
+  watch("./sass/**/*.scss", sassTask);
+}
 
-    return gulp.src('./css/*.css')
-        .pipe(sourcemaps.init())
-        .pipe(postcss([ autoprefixer({
-            browsers:['ie 7-11','Firefox > 20', 'last 2 versions' ]
-        }) ]))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./dest'));
-});
-
-gulp.task('watch', function () {
-    gulp.watch('./sass/**/*.scss', ['sass']);
-});
-
-gulp.task('default', function () {
-    // 将你的默认的任务代码放在这
-    gulp.start('sprite');
-});
+// 导出任务
+exports.sprite = spriteTask;
+exports.sass = sassTask;
+exports.postcss = postcssTask;
+exports.watch = watchTask;
+exports.default = series([spriteTask, sassTask, postcssTask]); // 默认任务
